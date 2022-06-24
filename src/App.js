d2d520c0fb57c6090856f0e2eacc7da5a6c392ab
shinkyung <ngogiahai014@gmail.com>
2022-06-24 15:29:33 +0700

    logo, username and password

--- src/App.js
+++ src/App.js
@@ -6,9 +6,22 @@ function App() {
     <div class="img-bg">
         <img src="./image1.jpg" alt = "Hình Ảnh Minh Họa"/>
     </div>
+    <div class="icon">
+        <img src="./BookStore.png" alt = "Logo"/>
+    </div>
     <div class="noi-dung">
         <div class="form">
             <h2>Sign in</h2>
+            <form action="">
+                <div class="input-form">
+                    <span>Username</span>
+                    <input type="text" name=""/>
+                </div>
+                <div class="input-form">
+                    <span>Password</span>
+                    <input type="password" name=""/>
+                </div>
+            </form>
         </div>
     </div>
   </section>
