24affeb515e88e9adece4425aeb386b0b8dcffdc
shinkyung <ngogiahai014@gmail.com>
2022-06-24 14:42:54 +0700

    image and sign in

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <section>
+    <div class="img-bg">
+        <img src="./image1.jpg" alt = "Hình Ảnh Minh Họa"/>
     </div>
+    <div class="noi-dung">
+        <div class="form">
+            <h2>Sign in</h2>
+        </div>
+    </div>
+  </section>
   );
 }
 
