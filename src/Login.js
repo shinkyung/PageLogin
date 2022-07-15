f7c7f64fd7b98d4e3c233b9fb2313158190d0f6b
shinkyung <ngogiahai014@gmail.com>
2022-07-15 21:27:52 +0700

    load database

--- src/Login.js
+++ src/Login.js
@@ -1,54 +1,67 @@
 import './Login.css';
+import axios from 'axios';
+import { useEffect, useState } from "react";
 
 function Login() {
-  return (
-  <section>
-    <div class="img-bg">
-        <img src="./image.jpg" alt = "Picture"/>
-    </div>
-    <div class="icon">
-        <img src="./BookStore.png" alt = "Logo"/>
-    </div>
-    <div class="content">
-        <div class="form">
-            <h2>Sign in</h2>
-            <form action="">
-                <div class="input">
-                    <span>Username</span>
-                    <input type="text" name=""/>
+    const [account, setAccount] = useState("");
+  
+    useEffect(() => {
+        const fetchAccount = async () => {
+            const res = await axios.get("http://localhost:5050/user");
+            setAccount(res.data);
+        };
+        fetchAccount();
+    }, []);
+
+    
+    return (
+    <section>
+        <div className = "img-bg">
+            <img src = "./image.jpg" alt = "Picture"/>
+        </div>
+        <div className = "icon">
+            <img src = "./BookStore.png" alt = "Logo"/>
+        </div>
+        <div className = "content">
+            <div className = "form">
+                <h2> Sign in </h2>
+                <form action = "">
+                    <div className = "input">
+                        <span> Username </span>
+                        <input type = "text" name = ""/>
+                    </div>
+                    <div className = "input">
+                        <span> Password </span>
+                        <input type = "password" name = ""/>
+                    </div>
+                    <div className = "remember">
+                        <label><input type="checkbox" name = ""/> Remember username </label>
+                    </div>
+                    <div className = "input">
+                        <input type="submit" value="Login"/>
+                    </div>
+                    <div className = "input">
+                        <p><a href="/#"> Sign up </a></p>
+                    </div>
+                    <div className = "input">
+                        <p><a href = "/#"> Forget password </a></p>
+                    </div>
+                </form>
+                <h3> Login With </h3>
+                <ul className = "icon">
+                    <li><i className = "fa fa-facebook" aria-hidden = "true"></i></li>
+                    <li><i className = "fa fa-google" aria-hidden = "true"></i></li>
+                    <li><i className = "fa fa-twitter" aria-hidden = "true"></i></li>
+                </ul>
+                <div className = "footer">
+                    &copy;
+                    2022
+                    Bookaholic Inc
                 </div>
-                <div class="input">
-                    <span>Password</span>
-                    <input type="password" name=""/>
-                </div>
-                <div class="remember">
-                    <label><input type="checkbox" name=""/> Remember username</label>
-                </div>
-                <div class="input">
-                    <input type="submit" value="Login"/>
-                </div>
-                <div class="input">
-                    <p><a href="/#">Sign up</a></p>
-                </div>
-                <div class="input">
-                    <p><a href="/#">Forget password </a></p>
-                </div>
-            </form>
-            <h3>Login With</h3>
-            <ul class="icon">
-                <li><i class="fa fa-facebook" aria-hidden="true"></i></li>
-                <li><i class="fa fa-google" aria-hidden="true"></i></li>
-                <li><i class="fa fa-twitter" aria-hidden="true"></i></li>
-            </ul>
-            <div class='footer'>
-                &copy;
-                2022
-                Bookaholic Inc
             </div>
         </div>
-    </div>
-  </section>
-  );
+    </section>
+    );
 }
 
 export default Login;
